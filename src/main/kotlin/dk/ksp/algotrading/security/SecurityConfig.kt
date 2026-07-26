1a34ebd5744ff8ef662079a142f2a8aa09f1c90d
Kasper Søballe Poulsen <kaspersoballe@gmail.com>
2026-07-26 14:52:20 +0200

    impliment security filter chain

--- src/main/kotlin/dk/ksp/algotrading/security/SecurityConfig.kt
+++ src/main/kotlin/dk/ksp/algotrading/security/SecurityConfig.kt
@@ -2,17 +2,46 @@ package dk.ksp.algotrading.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.config.http.SessionCreationPolicy
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig(
     private val traderDetailsService: TraderDetailsService,
 ) {
+
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http {
+            csrf {
+                disable()
+            }
+
+            authorizeHttpRequests {
+                authorize(HttpMethod.GET, "/api/saxo/oauth/callback", permitAll)
+                authorize(anyRequest, authenticated)
+            }
+
+            httpBasic { }
+
+            sessionManagement {
+                sessionCreationPolicy = SessionCreationPolicy.STATELESS
+            }
+        }
+
+        return http.build()
+    }
+
 
     @Bean
     fun authenticationProvider(passwordEncoder: PasswordEncoder): AuthenticationProvider =
